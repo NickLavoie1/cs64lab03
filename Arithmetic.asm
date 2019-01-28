119646c7dacbe99c7c24e1205e0374bd211269ab
NickLavoie1 <35705872+NickLavoie1@users.noreply.github.com>
2019-01-27 16:17:11 -0800

    Add files via upload

--- Arithmetic.asm
+++ Arithmetic.asm
@@ -5,7 +5,32 @@
 .text
 main:
 
-	# TODO: Write your code here
+	# Take integer input from user, store in t0, t1, and t2
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	li $v0, 5
+	syscall
+	move $t1, $v0
+	li $v0, 5
+	syscall
+	move $t2, $v0
+
+	# Multiply C by 3, store result into C
+	li $t3, 3
+	mult $t2, $t3
+	mflo $t2
+
+	# Add A and B, multiply result by 2, then subtract result by C
+	add $t0, $t0, $t1
+	add $t0, $t0, $t0
+	sub $t0, $t0, $t2
+
+	# Print result
+	li $v0, 1
+	move $a0, $t0
+	syscall
+
 
 exit:
 	# Exit
--- hello.asm
+++ hello.asm
@@ -3,13 +3,46 @@
 #
 #  Data Area - allocate and initialize variables
 .data
-	# TODO: Write your string definitions here
+	prompt: .asciiz "Give me an integer number between 0 and 1000:\n"
+	hello_world: .asciiz "Hello World!\n"
+	out_1: .asciiz "User chose "
+	out_2: .asciiz ". But is it the right answer?!"
+
 
 #Text Area (i.e. instructions)
 .text
 main:
+	# Print first statement
+	li $v0, 4
+	la $a0, prompt
+	syscall
 
-	# TODO: Write your code here
+	# Take in user integer input 
+	li $v0, 5
+	syscall
+	move $t0, $v0
+
+	# Print Hello World
+	li $v0, 4
+	la $a0, hello_world
+	syscall
+
+	# Print first part of output
+	li $v0, 4
+	la $a0, out_1
+	syscall
+
+	# Fill user input into output
+	li $v0, 1
+	move $a0, $t0
+	syscall
+
+	# Print second part of output
+	li $v0, 4
+	la $a0, out_2
+	syscall
+
+
 
 exit:
 	# Exit
